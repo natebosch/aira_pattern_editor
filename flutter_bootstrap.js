bd3bfe8d2671e43d69307e38948b45473f9e549c
Nate Bosch <nbosch1@gmail.com>
2025-07-09 18:13:18 -0600

    Built web
    Branch: main
    Commit: 8120483b0e3e6ab16d143c718a06cb2f5e8dae8c (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"03ef1ba910cac387f7b2af8ab09ca955d39746
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "515016268"
+    serviceWorkerVersion: "4069377927"
   }
 });
